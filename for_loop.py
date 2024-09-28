952fa75049491456824f615f3540830fd04a632e
Li <175231748+linkmodo@users.noreply.github.com>
2024-09-28 18:27:14 -0400

    Add files via upload
    looping exercise lab from introduction to python

---
+++ for_loop.py
@@ -0,0 +1,13 @@
+sum = 0
+'''
+for count in [1,2,3]:
+    num = int(input("Enter a number: "))
+    sum = sum + num
+average = sum / count
+
+print (f'The sum is {sum}')
+print (f'The average is {average}')
+'''
+
+for count in range(0, 8, 2):
+    print(count)
